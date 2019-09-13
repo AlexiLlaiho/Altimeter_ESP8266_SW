7a160feca3b8fc962d34b698d500be0536c78882
Alexander <alexillaiho@gmail.com>
2019-09-13 17:59:46 +0300

    was added  the common parts of code

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
  
 #include <ESP8266WiFi.h>
 #include <FS.h>   //Include File System Headers
- 
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
 const char* filename = "/samplefile.txt";
 int z = 251; 
   
@@ -13,6 +16,13 @@ void setup() {
   delay(1000);
   Serial.begin(115200);
   Serial.println();
+
+  Wire.pins(4, 5); //sda scl
+  Wire.begin(4, 5);
+  if (!bmp.begin()) {
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	while (1) {}
+  }
  
   //Initialize File System
   if(SPIFFS.begin())
@@ -58,7 +68,39 @@ void setup() {
   }
   //Serial.println("prepare of test-file has complete");
 }
- 
+
+void Calculate_Altitude()
+{
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  //Serial.println(" *C");
+
+  Serial.print("  Pressure = ");
+  Serial.print(bmp.readPressure());
+  //Serial.println(" Pa");
+
+  // Calculate altitude assuming 'standard' barometric
+  // pressure of 1013.25 millibar = 101325 Pascal
+  Serial.print("  Altitude = ");
+  Serial.print(bmp.readAltitude());
+  //Serial.println(" meters");
+
+  Serial.print("  Pressure at sealevel (calculated) = ");
+  Serial.print(bmp.readSealevelPressure());
+  //Serial.print(" Pa");
+
+  // you can get a more precise measurement of altitude
+  // if you know the current sea level pressure which will
+  // vary with weather and such. If it is 1015 millibars
+  // that is equal to 101500 Pascals.
+  Serial.print("  Real altitude = ");
+  Serial.print(bmp.readAltitude(102276));
+  Serial.println(" meters");
+
+  Serial.println();
+  delay(500);
+}
+
 void loop() {
   int i;
   
