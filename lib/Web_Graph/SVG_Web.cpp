88b8041e056208e7cd2ccc334835fb5400b003f6
alexillaiho <alexillaiho@gmail.com>
2021-03-06 15:32:45 +0300

    void SVG_Graph()
    total rebuild of graph view

--- lib/Web_Graph/SVG_Web.cpp
+++ lib/Web_Graph/SVG_Web.cpp
@@ -117,40 +117,62 @@ void SVG_Graph()
       case 12: a = 2860; ++GrPart; break;
       case 13: a = 3100; ++GrPart; break;
       case 14: a = 3340; ++GrPart; break;
-    }  
-    out += " <svg width=\"980\" height=\"2750\" xmlns=\"http://www.w3.org/2000/svg\">\n";
+    }
+out += "<html lang=\"en\">\n";
+  out += "<head> <meta charset=\"UTF-8\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> <style> body {color: black;  margin: 2% auto;} @media screen and (max-width: 1085px) {body{width: 18%; }} @media screen and (max-width: 642px) {body{width: 45%; }} </style> </head>\n";
+    out += "<body> \n";
+    out += " <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 980 2750\" preserveAspectRatio=\"xMidYMid meet\">\n";
     out += " <g>\n";
-    out += " <title>background</title>\n";
-    out += " <rect fill=\"#fff\" id=\"canvas_background\" height=\"2752\" width=\"982\" y=\"-1\" x=\"-1\"/>\n";
-    out += " <g display=\"none\" overflow=\"visible\" y=\"0\" x=\"0\" height=\"100%\" width=\"100%\" id=\"canvasGrid\">\n";
-    out += " <rect fill=\"url(#gridpattern)\" stroke-width=\"0\" y=\"0\" x=\"0\" height=\"100%\" width=\"100%\"/>\n";
-    out += " </g>\n";
+      out += " <title>background</title>\n";
+        out += " <rect fill=\"#fff\" id=\"canvas_background\" height=\"2752\" width=\"982\" y=\"-1\" x=\"-1\"/>\n";
+          out += " <g display=\"none\" overflow=\"visible\" y=\"0\" x=\"0\" height=\"100%\" width=\"100%\" id=\"canvasGrid\">\n";
+            out += " <rect fill=\"url(#gridpattern)\" stroke-width=\"0\" y=\"0\" x=\"0\" height=\"100%\" width=\"100%\"/>\n";
+        out += " </g>\n";
     out += " </g>\n";
     out += " <g>\n";
-    out += " <title>Layer 1</title>\n";
-    out += " <line stroke-linecap=\"undefined\" stroke-linejoin=\"undefined\" id=\"svg_1\" y2=\"2630\" x2=\"80\" y1=\"30\" x1=\"80\" stroke-width=\"1\" stroke=\"#000\" fill=\"none\"/>\n";
-    out += " <line stroke-linecap=\"undefined\" stroke-linejoin=\"undefined\" id=\"svg_2\" y2=\"2630\" x2=\"950\" y1=\"2630\" x1=\"60\" stroke-width=\"1\" stroke=\"#000\" fill=\"none\"/>\n";
-    out += " <text font-size=\"36\" id=\"svg_3\" y=\"2660\" x=\"5\" stroke-width=\"2\">0</text>\n";
-    out += " <text font-size=\"36\" id=\"svg_8\" y=\"2530\" x=\"5\" stroke-width=\"2\">10</text>\n";
-    out += " <line stroke-dasharray=\"2,2\" id=\"svg_10\" y2=\"2530\" x2=\"950\" y1=\"2530\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
-    out += " <text font-size=\"36\" id=\"svg_6\" y=\"2330\" x=\"5\"  stroke-width=\"3\">30</text>\n";
-    out += " <line stroke-dasharray=\"2,2\" id=\"svg_8\" y2=\"2330\" x2=\"950\" y1=\"2330\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
-    out += " <text font-size=\"36\" id=\"svg_5\" y=\"1315\" x=\"5\"  stroke-width=\"3\">130</text>\n";
-    out += " <text font-size=\"36\" id=\"svg_7\" y=\"640\"  x=\"5\"  stroke-width=\"3\">195</text>\n";
-    out += " <text font-size=\"36\" id=\"svg_4\" y=\"30\"   x=\"5\"  stroke-width=\"2\">260</text>\n";
+        out += " <title>Layer 1</title>\n";
+        out += " <line stroke-linecap=\"undefined\" stroke-linejoin=\"undefined\" id=\"svg_1\" y2=\"2630\" x2=\"80\" y1=\"30\" x1=\"80\" stroke-width=\"1\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += " <line stroke-linecap=\"undefined\" stroke-linejoin=\"undefined\" id=\"svg_2\" y2=\"2630\" x2=\"950\" y1=\"2630\" x1=\"60\" stroke-width=\"1\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += " <text font-size=\"36\" y=\"2630\" x=\"5\" stroke-width=\"2\">0</text>\n";
+        out += " <text font-size=\"36\" y=\"2530\" x=\"5\" stroke-width=\"2\">10</text>\n";
+        out += " <line stroke-dasharray=\"2,2\" y2=\"2530\" x2=\"950\" y1=\"2530\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += " <text font-size=\"36\" y=\"2330\" x=\"5\"  stroke-width=\"3\">30</text>\n";
+        out += " <line stroke-dasharray=\"2,2\" y2=\"2330\" x2=\"950\" y1=\"2330\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += " <text font-size=\"36\" y=\"2130\" x=\"15\"  stroke-width=\"3\">50</text>\n";
+        out += " <line stroke-dasharray=\"2,2\" y2=\"2130\" x2=\"950\" y1=\"2130\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += " <text font-size=\"36\" y=\"1930\" x=\"15\" stroke-width=\"3\">70</text>\n";
+        out += " <line stroke-dasharray=\"2,2\" y2=\"1930\" x2=\"950\" y1=\"1930\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += " <text font-size=\"36\" y=\"1730\" x=\"15\"  stroke-width=\"3\">90</text>\n";
+        out += " <line stroke-dasharray=\"2,2\" y2=\"1730\" x2=\"950\" y1=\"1730\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += " <text font-size=\"36\" y=\"1530\" x=\"5\"  stroke-width=\"3\">110</text>\n";
+        out += " <line stroke-dasharray=\"2,2\" y2=\"1530\" x2=\"950\" y1=\"1530\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += "<text font-size=\"36\" y=\"1330\" x=\"5\" stroke-width=\"3\">130</text>\n";
+        out += "<line stroke-dasharray=\"2,2\" y2=\"1330\" x2=\"950\" y1=\"1330\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += "<text font-size=\"36\" y=\"1130\" x=\"5\" stroke-width=\"3\">150</text>\n";
+        out += "<line stroke-dasharray=\"2,2\" y2=\"1130\" x2=\"950\" y1=\"1130\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += "<text font-size=\"36\" y=\"930\" x=\"5\" stroke-width=\"3\">170</text>\n";
+        out += "<line stroke-dasharray=\"2,2\" y2=\"930\" x2=\"950\" y1=\"930\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += "<text font-size=\"36\" y=\"730\" x=\"5\" stroke-width=\"3\">190</text>\n";
+        out += "<line stroke-dasharray=\"2,2\" y2=\"730\" x2=\"950\" y1=\"730\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += "<text font-size=\"36\" y=\"530\" x=\"5\" stroke-width=\"3\">210</text>\n";
+        out += "<line stroke-dasharray=\"2,2\" y2=\"530\" x2=\"950\" y1=\"530\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += "<text font-size=\"36\" y=\"330\" x=\"5\" stroke-width=\"3\">230</text>\n";
+        out += "<line stroke-dasharray=\"2,2\" y2=\"330\" x2=\"950\" y1=\"330\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
+        out += "<text font-size=\"36\" y=\"130\" x=\"5\" stroke-width=\"3\">250</text>\n";
+        out += "<line stroke-dasharray=\"2,2\" y2=\"130\" x2=\"950\" y1=\"130\" x1=\"80\" stroke-width=\"2\" stroke=\"#000\" fill=\"none\"/>\n";
 
-    out += " <text font-size=\"36\" id=\"svg_59\" y=\"2690\" x=\"30\">\n ";
-    sprintf(temp, "Давление в точке старта (Па): %d", int(dPS));
-    out += temp;
-    out += " </text>\n";
-    out += " <text font-size=\"36\" id=\"svg_61\" y=\"2730\" x=\"30\">\n ";
-    sprintf(temp, "Максимальная высота (м): %f", HL);
-    out += temp;
-    out += " </text>\n";
-    out += " <text font-size=\"36\" id=\"svg_62\" y=\"2730\" x=\"712\">\n ";
-    sprintf(temp, "Страница: %d", GrPart);
-    out += temp;
-    out += " </text>\n";
+        out += " <text font-size=\"36\" id=\"svg_59\" y=\"2690\" x=\"30\">\n ";
+          sprintf(temp, "Давление в точке старта (Па): %d", int(dPS));
+          out += temp;
+        out += " </text>\n";
+        out += " <text font-size=\"36\" id=\"svg_61\" y=\"2730\" x=\"30\">\n ";
+          sprintf(temp, "Максимальная высота (м): %f", HL);
+          out += temp;
+        out += " </text>\n";
+        out += " <text font-size=\"36\" id=\"svg_62\" y=\"2730\" x=\"712\">\n ";
+          sprintf(temp, "Страница: %d", GrPart);
+          out += temp;
+        out += " </text>\n";
     out += " </g>\n ";
     out += "<g stroke=\"black\">\n";
     String outT = "";
@@ -170,10 +192,10 @@ void SVG_Graph()
     }
     if (GrPart == 15) GrPart = 0;
     out += "</g>\n";
-
     out += "</svg>\n";
-
-  server.send(200, "image/svg+xml", out);
+  out += "</body> \n";
+out += "</html>\n";
+server.send(200, "html", out); //image/svg+xml
 }
 
 void fTurnOFF()
